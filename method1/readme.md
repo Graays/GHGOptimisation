96324a955d54bb705d96cad46780ca781e2cf716
Graays <j.granier@hotmail.fr>
2021-04-03 14:16:23 +0200

    Adding Score Calc Function
    Adding Score Calculation function in utils/score.py
    + small fixes

--- method1/readme.md
+++
@@ -1,5 +0,0 @@
-Version dont le développement a commencé durant le Hackathon
-
-- Version 1 : Version du Hackathon
-- Version 1.1 : Optimisation du code du Hackathon
-- 
--- utils/score.py
+++ utils/score.py
@@ -1,32 +1,34 @@
 from csvreader import *
 
-def score(file,duration):
+def score(output,duration):
 
-    catalog = """tiny1,2000,100
-tiny2,3000,150
-tiny3,2500,100
-classic1,6000,1000
-classic2,11000,2000
-classic3,6000,3000
-classic4,15000,3500
-classic5,12000,3000
-classic6,11500,3000
-large1,25000,10000
-large2,25000,8000
-large3,40000,15000
-storage1,13000,600
-storage2,10000,300
-storage3,15000,1000
-compute1,12000,4000
-compute2,13000,4500
-compute3,16200,6000
-memory1,10000,1000
-memory2,14000,1300
-"""
+    score = 0
 
-    servers = csvreader(file)
+    catalog = [['tiny1',2000,100],
+               ['tiny2', 3000, 150],
+               ['tiny3', 2500, 100],
+               ['classic1', 6000, 1000],
+               ['classic2', 11000, 2000],
+               ['classic3', 6000, 3000],
+               ['classic4', 15000, 3500],
+               ['classic5', 12000, 3000],
+               ['classic6', 11500, 3000],
+               ['large1', 25000, 10000],
+               ['large2', 25000, 8000],
+               ['large3', 40000, 15000],
+               ['storage1', 13000, 600],
+               ['storage2', 10000, 300],
+               ['storage3', 15000, 1000],
+               ['compute1', 12000, 4000],
+               ['compute2', 13000, 4500],
+               ['compute3', 16200, 6000],
+               ['memory1', 10000, 1000],
+               ['memory2', 14000, 1300]]
 
-    for server in servers:
+    output = csvreader(output)
+    for i in range(len(output)):
+        for j in range(len(catalog)):
+            if output[i][0] == catalog[j][0]:
+                score += catalog[j][1] + duration * catalog[j][2]
 
-
-
+    return score
