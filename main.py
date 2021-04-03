2dc77c3bca1c0d951bab199eb1e7f184439a380b
Graays <j.granier@hotmail.fr>
2021-04-03 13:58:47 +0200

    Typo + Structuring

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import time
+import subprocess
+import sys
 
 from utils.csvreader import *
 
-req = 0
+req = 1
+
 while(not(req >=1 and req <= 6)):
     req = input("Select a dataset between 1 and 6 : \n")
     try:
@@ -11,7 +14,15 @@ while(not(req >=1 and req <= 6)):
         print("Valuetype 'int' expected")
         req = 0
 
-req = str(req)
+meth = 0
+while(not(meth >=1 and meth <= 1)):
+    req = input("Select a method between 1 and # : \n")
+    try:
+        meth = int(req)
+    except:
+        print("Valuetype 'int' expected")
+        meth = 0
+
 # Starting the clock
 tic = time.time()
 
@@ -21,7 +32,10 @@ servers = csvreader('data/servers_catalog.csv')
 # "requests list represents the list of services that needs to be sorted through servers
 requests = csvreader('data/ctstfr0280_input_{}.csv'.format(req))
 
-print(requests)
+
+
+print(subprocess.run([sys.executable,'methods/method{}/resolution.py'.format(meth)]))
+
 
 toc = time.time()
 print("Time elapsed : {} seconds".format(toc-tic))
---
+++ utils/score.py
@@ -0,0 +1,32 @@
+from csvreader import *
+
+def score(file,duration):
+
+    catalog = """tiny1,2000,100
+tiny2,3000,150
+tiny3,2500,100
+classic1,6000,1000
+classic2,11000,2000
+classic3,6000,3000
+classic4,15000,3500
+classic5,12000,3000
+classic6,11500,3000
+large1,25000,10000
+large2,25000,8000
+large3,40000,15000
+storage1,13000,600
+storage2,10000,300
+storage3,15000,1000
+compute1,12000,4000
+compute2,13000,4500
+compute3,16200,6000
+memory1,10000,1000
+memory2,14000,1300
+"""
+
+    servers = csvreader(file)
+
+    for server in servers:
+
+
+
